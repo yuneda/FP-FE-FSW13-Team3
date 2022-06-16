547de6ed5f1ec34e9f7099e586d77b33db4b2522
Ahmad Ilham <angilham2@gmail.com>
2022-06-16 20:32:25 +0700

    [Ilham] add eye and eyeOff icon in input form

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Icon } from "react-icons-kit";
+import { eye } from "react-icons-kit/feather/eye";
+import { eyeOff } from "react-icons-kit/feather/eyeOff";
 import BgLogin from "../../../../src/assets/bg-login.png";
 import "./Login.css";
 
 const Login = () => {
   var sectionStyle = {
     backgroundImage: "url(" + BgLogin + ")",
+    backgroundRepeat: "no-repeat",
+  };
+
+  const [type, setType] = useState("password");
+  const [icon, setIcon] = useState(eyeOff);
+
+  const handleToggle = () => {
+    event.preventDefault();
+    if (type === "password") {
+      setIcon(eye);
+      setType("text");
+    } else {
+      setIcon(eyeOff);
+      setType("password");
+    }
   };
   return (
     <div className="container-fluid box">
@@ -25,11 +43,11 @@ const Login = () => {
         <div className="col-md-6 col-sm-12 col-12 right d-flex align-items-center">
           <div>
             <div className="row w-100 justify-content-center">
-              <div className="col-10 mb-3">
+              <div className="col-9 mb-3">
                 <i className="bi bi-arrow-left" />
                 <h1>Masuk</h1>
               </div>
-              <div className="col-10">
+              <div className="col-9">
                 <label htmlFor>Email</label>
                 <div className="input-group mb-3">
                   <input
@@ -42,25 +60,32 @@ const Login = () => {
                   />
                 </div>
               </div>
-              <div className="col-10">
-                <label htmlFor>Password</label>
-                <div className="input-group mb-3">
-                  <input
-                    type="password"
-                    className="border-radius form-control"
-                    placeholder="Username"
-                    aria-label="Username"
-                    aria-describedby="basic-addon1"
-                    autoComplete="off"
-                  />
+              <div className="col-9">
+                <label htmlFor="" className="d-flex justify-content-between">
+                  Password
+                </label>
+                <div className="input-group mb-3 wrapper">
+                  <div className="input-field">
+                    <input
+                      type={type}
+                      className="border-radius form-control"
+                      placeholder="Username"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                      autoComplete="off"
+                    />
+                    <button onClick={handleToggle}>
+                      <Icon icon={icon} size={20} />
+                    </button>
+                  </div>
                 </div>
               </div>
-              <div className="col-10 mb-5">
+              <div className="col-9 mb-5">
                 <button className="btn w-100 border-radius btn-login">
                   Masuk
                 </button>
               </div>
-              <div className="col-10 text-center">
+              <div className="col-9 text-center">
                 <p>
                   Belum punya akun ?{" "}
                   <a
