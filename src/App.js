dffb1305e6d0a07fc24298371b7ae6594a7711cd
alfajryuneda <alfajryuneda@gmail.com>
2022-06-18 17:40:37 +0700

    [Yuneda] add seller product page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/pages/Login/Login";
-import Register from "./components/pages/Register/Register";
-import Home from "./components/pages/Home/Home";
-import Profile from "./components/pages/Profile/Profile";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/pages/Login/Login';
+import Register from './components/pages/Register/Register';
+import Home from './components/pages/Home/Home';
+import Profile from './components/pages/Profile/Profile';
+import Product from './components/pages/Product/Product';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/molecules/productlist/ProductList.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import userImage from '../../../assets/user.jpg';
+
+const ProductList = () => {
+  const imgStyle = {
+    height: '100px',
+    objectFit: 'cover',
+  };
+  return (
+    <>
+      <div className="row justify-content-start g-1 row-cols-lg-3">
+        <div className="col">
+          <button className="add-product">
+            <i class="fa-solid fa-plus"></i>
+          </button>
+        </div>
+        <div className="col">
+          <div className="card p-2">
+            <img src={userImage} style={imgStyle} />
+            <p className="product-title mb-0">Jam Tangan Casio</p>
+            <p className="desc mb-0">Aksesoris</p>
+            <p className="price">RP 250.000</p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card p-2">
+            <img src={userImage} style={imgStyle} />
+            <p className="product-title mb-0">Jam Tangan Casio</p>
+            <p className="desc mb-0">Aksesoris</p>
+            <p className="price">RP 250.000</p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card p-2">
+            <img src={userImage} style={imgStyle} />
+            <p className="product-title mb-0">Jam Tangan Casio</p>
+            <p className="desc mb-0">Aksesoris</p>
+            <p className="price">RP 250.000</p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card p-2">
+            <img src={userImage} style={imgStyle} />
+            <p className="product-title mb-0">Jam Tangan Casio</p>
+            <p className="desc mb-0">Aksesoris</p>
+            <p className="price">RP 250.000</p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card p-2">
+            <img src={userImage} style={imgStyle} />
+            <p className="product-title mb-0">Jam Tangan Casio</p>
+            <p className="desc mb-0">Aksesoris</p>
+            <p className="price">RP 250.000</p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card p-2">
+            <img src={userImage} style={imgStyle} />
+            <p className="product-title mb-0">Jam Tangan Casio</p>
+            <p className="desc mb-0">Aksesoris</p>
+            <p className="price">RP 250.000</p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card p-2">
+            <img src={userImage} style={imgStyle} />
+            <p className="product-title mb-0">Jam Tangan Casio</p>
+            <p className="desc mb-0">Aksesoris</p>
+            <p className="price">RP 250.000</p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card p-2">
+            <img src={userImage} style={imgStyle} />
+            <p className="product-title mb-0">Jam Tangan Casio</p>
+            <p className="desc mb-0">Aksesoris</p>
+            <p className="price">RP 250.000</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductList;
