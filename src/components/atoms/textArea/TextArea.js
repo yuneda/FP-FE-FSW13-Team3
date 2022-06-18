585863b530d4324c220dec10766565c4241ff6cb
Ahmad Ilham <angilham2@gmail.com>
2022-06-18 17:14:59 +0700

    [Ilham] add input atom component

---
+++ src/components/atoms/textArea/TextArea.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+
+const TextArea = (props) => {
+  return (
+    <>
+      <div className="col-9" as="textarea">
+        <label className="d-flex justify-content-between">{props.name}</label>
+        <div className="input-group mb-3">
+          <Form.Control
+            as={props.as}
+            rows={3}
+            type={props.type}
+            className="border-radius form-control"
+            placeholder={props.placeholder}
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            autoComplete="off"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TextArea;
