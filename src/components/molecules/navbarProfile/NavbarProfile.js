8ee9e8e00210ae010f043c3649c39894d79155e1
Ahmad Ilham <angilham2@gmail.com>
2022-06-26 13:15:01 +0700

    [Ilham] add responsive in mobile page

--- src/components/molecules/navbarProfile/NavbarProfile.js
+++ src/components/molecules/navbarProfile/NavbarProfile.js
@@ -12,33 +12,26 @@ function MyNavbar(props) {
     top: "27px",
     background: "#4B1979",
   };
+
   return (
-    <Navbar
-      expand="lg"
-      style={{
-        borderBottom: "3px solid #EEEEEE",
-      }}
-    >
+    <Navbar expand="lg" className="fox">
       <Container>
         <div className="logo"></div>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <div className="col-4">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <div style={logo} className="mt-2"></div>
-            </Nav>
-          </div>
-          <div className="col-4 ">
-            <Navbar.Text className="justify-content-center d-flex">
-              {props.title}
-            </Navbar.Text>
-          </div>
-          <div className="col-4"></div>
-        </Navbar.Collapse>
+        <div className="col-sm-4">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <div style={logo} className="mt-2 logo"></div>
+          </Nav>
+        </div>
+        <div className="col-sm-4">
+          <Navbar.Text className="justify-content-center d-flex">
+            {props.title}
+          </Navbar.Text>
+        </div>
+        <div className="col-sm-4"></div>
       </Container>
     </Navbar>
   );
--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -1,23 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Profile.css";
 import Navbar from "../../molecules/navbarProfile/NavbarProfile";
 import Form from "react-bootstrap/Form";
 import PicInput from "../../../assets/fi_camera.png";
+import axios from "axios";
+import city from "../../../docs/city.json";
 
 const Profile = () => {
+  const url =
+    "https://fp-be-fsw13-tim3.herokuapp.com/api/v1/users/:id/picture/cloudinary";
+  const token = localStorage.getItem("token");
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const [name, setName] = useState("");
+  const [kota, setKota] = useState("");
+  const [alamat, setAlamat] = useState("");
+  const [no_tlpn, setNo_tlpn] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleName = (event) => {
+    event.preventDefault();
+    setName(event.target.value);
+  };
+  const handleKota = (event) => {
+    event.preventDefault();
+    setKota(event.target.value);
+  };
+  const handleAlamat = (event) => {
+    event.preventDefault();
+    setAlamat(event.target.value);
+  };
+  const handleTelepon = (event) => {
+    event.preventDefault();
+    setNo_tlpn(event.target.value);
+  };
+  const handleImage = (event) => {
+    event.preventDefault();
+    setImage(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.put(
+        url,
+        {
+          name,
+          city: kota,
+          address: alamat,
+          no_tlpn,
+        },
+        config
+      );
+      console.log(name, kota, alamat, no_tlpn_image);
+      setName("");
+      setKota("");
+      setAlamat("");
+      setNo_tlpn("");
+      setImage("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <Navbar />
+      <Navbar title="Lengkapi Info Akun" />
       <div className="container-fluid box">
         <div className="row justify-content-center mt-3">
           <div className="col-md-6 col-sm-12 col-12 right d-flex">
-            <form autoComplete="off">
-              <div className="row w-100 justify-content-center ">
+            <form
+              autoComplete="off"
+              onSubmit={handleSubmit}
+              encType="multipart/form-data"
+              className="justify-content-center d-flex"
+            >
+              <div className="row w-100 justify-content-center fit">
                 <i
-                  className="fa-solid fa-arrow-left"
+                  className="fa-solid fa-arrow-left fit"
                   style={{ marginTop: "20px" }}
                 ></i>
-                <div className="col-4  justify-content-center d-flex">
+                <div className="col-sm-4  justify-content-center d-flex">
                   <div className="bgImage ">
                     <label
                       className="d-flex justify-content-center image-label"
@@ -29,18 +96,20 @@ const Profile = () => {
                       <input
                         id="input-file"
                         type="file"
-                        name="myImage"
+                        name="picture"
                         accept="image/png, image/gif, image/jpeg"
                         className="border-radius form-control picture-field"
                         placeholder="Username"
                         aria-label="Username"
                         aria-describedby="basic-addon1"
                         autoComplete="off"
+                        value={image}
+                        onChange={handleImage}
                       />
                     </div>
                   </div>
                 </div>
-                <div className="col-9">
+                <div className="col-sm-9">
                   <label className="d-flex justify-content-between">
                     Nama*
                   </label>
@@ -52,27 +121,35 @@ const Profile = () => {
                       aria-label="Username"
                       aria-describedby="basic-addon1"
                       autoComplete="off"
+                      value={name}
+                      onChange={handleName}
                     />
                   </div>
                 </div>
-                <div className="col-9 mb-3 ">
+                <div className="col-sm-9 mb-3 ">
                   <label className="d-flex justify-content-between">
                     Kota*
                   </label>
-                  <div class="input-group ">
+                  <div className="input-group ">
                     <select
-                      class="form-select option-field"
+                      className="form-select option-field"
                       id="inputGroupSelect04"
                       aria-label="Example select with button addon"
                     >
                       <option selected>Pilih Kota</option>
-                      <option value="1">One</option>
+                      {city.map((town, index) => (
+                        <option key={index} value={town.city}>
+                          {town.city}
+                        </option>
+                      ))}
+
+                      {/* <option value="1">One</option>
                       <option value="2">Two</option>
-                      <option value="3">Three</option>
+                      <option value="3">Three</option> */}
                     </select>
                   </div>
                 </div>
-                <div className="col-9" as="textarea">
+                <div className="col-sm-9" as="textarea">
                   <label className="d-flex justify-content-between">
                     Alamat*
                   </label>
@@ -86,10 +163,12 @@ const Profile = () => {
                       aria-label="Username"
                       aria-describedby="basic-addon1"
                       autoComplete="off"
+                      value={alamat}
+                      onChange={handleAlamat}
                     />
                   </div>
                 </div>
-                <div className="col-9">
+                <div className="col-sm-9">
                   <label className="d-flex justify-content-between">
                     No Handphone*
                   </label>
@@ -101,11 +180,16 @@ const Profile = () => {
                       aria-label="Username"
                       aria-describedby="basic-addon1"
                       autoComplete="off"
+                      value={no_tlpn}
+                      onChange={handleTelepon}
                     />
                   </div>
                 </div>
-                <div className="col-9 mb-5">
-                  <button className="btn w-100 border-radius btn-register">
+                <div className="col-sm-9 mb-5">
+                  <button
+                    className="btn w-100 border-radius btn-register"
+                    type="submit"
+                  >
                     Simpan
                   </button>
                 </div>
