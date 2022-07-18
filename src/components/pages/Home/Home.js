7a3268972c9b86df0ba5da00cbfbfcd0a0cc0d51
yuneda <alfajryuneda@gmail.com>
2022-07-18 10:20:35 +0700

    [Refactor] Homepage

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -15,6 +15,8 @@ import DesktopView from '../Responsive/DesktopView';
 import { decodeToken, isExpired } from 'react-jwt';
 import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
 import { useMediaQuery } from 'react-responsive';
+import UserMenu from './molecules/UserMenu';
+import NotifDesktop from './molecules/NotifDesktop';
 
 let result;
 let address;
@@ -120,164 +122,13 @@ const Home = () => {
       />
       {/* </DesktopView> */}
       <div className="container position-relative">
-        <Toast
-          className={`${styles.cardNotif} p-1 bg-white`}
-          show={showMenu}
-          onClose={toggleMenu}
-        >
-          <Toast.Body>
-            <div className="fw-bold">Akun Saya</div>
-            <Link
-              to="/profile"
-              style={{ color: 'inherit', textDecoration: 'inherit' }}
-            >
-              <div>
-                <i
-                  className="fa-regular fa-bookmark me-2"
-                  style={{ color: '#7126B5' }}
-                ></i>
-                Ubah Akun
-              </div>
-            </Link>
-            <div>
-              <i
-                className="fa-solid fa-gear me-2"
-                style={{ color: '#7126B5' }}
-              ></i>
-              Pengaturan
-            </div>
-            <Link
-              to="/wishlist"
-              style={{ color: 'inherit', textDecoration: 'inherit' }}
-            >
-              <div>
-                <i
-                  className="fa-regular fa-bookmark me-2"
-                  style={{ color: '#7126B5' }}
-                ></i>
-                Daftar Simpan
-              </div>
-            </Link>
-            <div
-              onClick={(e) => {
-                e.preventDefault();
-                localStorage.removeItem('token');
-                navigate('/login');
-              }}
-              style={{ cursor: 'pointer' }}
-            >
-              <i
-                className="fa-regular fa-bookmark me-2"
-                style={{ color: '#7126B5' }}
-              ></i>
-              Keluar
-            </div>
-            <div className="row justify-content-center">Verson 1.0.0</div>
-          </Toast.Body>
-        </Toast>
-        <Toast
-          className={`${styles.cardNotif} p-1 bg-white`}
-          show={showA}
-          onClose={toggleShowA}
-        >
-          <Toast.Body>
-            <div className={``}>
-              <div className="row">
-                {notif &&
-                  notif.map((notif, index) => {
-                    if (notif.status == 'created') {
-                      address = 'product/' + notif.id_product;
-                    } else if (notif.id_buyer == idLogin) {
-                      address = '/';
-                    } else {
-                      address = `offer/${notif.id}`;
-                    }
-                    return (
-                      <Link
-                        key={index}
-                        // to={
-                        //   notif.id_buyer == idLogin ? '/' : `offer/${notif.id}`
-                        // }
-                        to={address}
-                        style={{ color: 'inherit', textDecoration: 'inherit' }}
-                      >
-                        <div key={index} className="row">
-                          <div className="col-3">
-                            <img
-                              src={notif.Product.image[0]}
-                              alt=""
-                              className={`${styles.productImg} img-fluid`}
-                            />
-                          </div>
-                          <div className="col-9 g-0">
-                            <div className="row">
-                              <div className="col-7 g-0  ps-3">
-                                <div className="text-secondary">
-                                  {notif.status == 'created' &&
-                                    'Berhasil diterbitkan'}
-                                  {notif.status !== 'created' &&
-                                    'Penawaran produk'}
-                                </div>
-                                <div className="fw-bold">
-                                  {notif.Product.product_name}
-                                </div>
-                                <div
-                                  className="fw-bold"
-                                  style={{
-                                    textDecoration:
-                                      notif.status == 'accept'
-                                        ? 'line-through'
-                                        : 'none',
-                                  }}
-                                >
-                                  {Intl.NumberFormat('id-ID', {
-                                    style: 'currency',
-                                    currency: 'IDR',
-                                  }).format(notif.Product.product_price)}
-                                </div>
-                                {notif.status !== 'created' && (
-                                  <div className="fw-bold">
-                                    Ditawar{' '}
-                                    {Intl.NumberFormat('id-ID', {
-                                      style: 'currency',
-                                      currency: 'IDR',
-                                    }).format(notif.Offer.bid_price)}
-                                  </div>
-                                )}
-                              </div>
-                              <div className="col-5 g-0 ">
-                                <div className="text-secondary">
-                                  20 Apr, 14:04{' '}
-                                  <i
-                                    className="fa-solid fa-circle fa-xs"
-                                    style={{
-                                      color: 'red',
-                                    }}
-                                  ></i>
-                                </div>
-                              </div>
-                              {notif.status == 'accept' && (
-                                <div className="text-secondary">
-                                  {idLogin !== notif.id_seller &&
-                                    'Kamu akan dihubungi via WA'}
-                                  {idLogin == notif.id_seller &&
-                                    'Kamu menerima penawaran ini'}
-                                </div>
-                              )}
-                            </div>
-                          </div>
-
-                          <div className="col-12">
-                            <hr />
-                          </div>
-                        </div>
-                      </Link>
-                    );
-                  })}
-              </div>
-            </div>
-          </Toast.Body>
-        </Toast>
+        <UserMenu showMenu={showMenu} toggleMenu={toggleMenu} />
+        <NotifDesktop
+          idLogin={idLogin}
+          notif={notif}
+          toggleShowA={toggleShowA}
+          showA={showA}
+        />
       </div>
       <div className={mobileView ? '' : 'mt-5'}>
         <MyCarousel />
